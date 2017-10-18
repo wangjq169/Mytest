2ed26eda55641e83cf9ec2528feaa7862069b8cd
wangjq169 <39447519@qq.com>
2017-10-18 22:55:26 +0800

    demo10

---
+++ demo1010/src/cat/Demo.java
@@ -0,0 +1,10 @@
+package cat;
+
+public class Demo {
+
+	static Demo demo=new Demo();
+	private void demo(){}
+	public static Demo getinance(){
+		return demo;
+	}
+}
---
+++ demo1010/src/cat/Test.java
@@ -0,0 +1,12 @@
+package cat;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Demo demo=Demo.getinance();
+	
+	}
+
+}
